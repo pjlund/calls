11e791d9de9ada4954097082e2384410f167ea54
Pete Lund <pete.lund@gmail.com>
2015-02-03 14:52:20 -0500

    Extract dates and add comments on subfunctions

--- analysis.py
+++ analysis.py
@@ -2,14 +2,15 @@
 #	PJL 2/3/2015
 import numpy as np
 import csv
+import datetime
 
 def main():
 
 	# preprocessing
 	my_list = csvToList()
 	call_times = getCallTimes(my_list)
-
-	print call_times
+	dates = getDates(my_list)
+	print dates
 # analysis
 
 ## distribution of call lengths
@@ -21,6 +22,7 @@ def main():
 ## call lengths by previous call length
 
 def csvToList():
+	# read csv into list
 
 	with open('PhoneTime.csv', 'rb') as f:
 		reader = csv.reader(f)
@@ -28,6 +30,8 @@ def csvToList():
 	return my_list
 
 def getCallTimes(my_list):
+	# return list of call times in minutes, one for each phone call
+
 	call_times = []
 	for i in range(len(my_list)):
 		if i == 0:
@@ -38,5 +42,23 @@ def getCallTimes(my_list):
 			s = float(my_list[i][3])
 			call_times.append(h*60.0 + m + s/60.0)
 	return call_times
+
+def getDates(my_list):
+	# return list of date objects, one for each phone call
+	# see https://docs.python.org/2/library/datetime.html
+	
+	dates = []
+	for i in range(len(my_list)):
+		if i == 0:
+			continue # skip header of csv
+		else:
+			date_MDY = my_list[i][0].split("/")
+			month = int(date_MDY[0])
+			day = int(date_MDY[1])
+			year = int(date_MDY[2])
+			
+			dates.append(datetime.date(year, month, day))
+
+	return dates
 
 main()
